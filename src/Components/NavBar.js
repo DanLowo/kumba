3ca6c4d58c14f38f7e0ffa94f1753b6d1e7ab41c
DanLowo <danlowo@gmail.com>
2021-09-19 10:44:44 +0100

    navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav id="navbar">
+      <h1>Kumba</h1>
+      <div id="nav-links">
+        <ul>
+          <li>Home</li>
+          <li>Profile</li>
+          <li>Orders</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
