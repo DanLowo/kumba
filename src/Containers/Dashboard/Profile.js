31f4a96591f629d1d9ac90351918cf40c1f23419
DanLowo <danlowo@gmail.com>
2021-09-21 13:15:44 +0100

    Get users' data from api

--- src/Containers/Dashboard/Profile.js
+++ src/Containers/Dashboard/Profile.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 
 // import components
 import UserImage from "../../asset/reddit.png"
 
 const ProfileContainer = () => {
+
+  const [user, setUser] = useState([])
+
+  const getUser = async () => {
+    const userArr = []
+    try {
+      // get users data from api
+      const { data : { user } } = await axios.get('https://indapi.kumba.io/webdev/assignment')
+
+      // make an array for all the keys and values of the user data
+      Object.entries(user).map((keyValue) => {
+        userArr.push({ title: keyValue[0], value: keyValue[1] })
+      })
+
+      // return new formatted array of users' data.
+      return userArr
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // eslint-disable-next-line
+  useEffect(async () => {
+    const newUser = await getUser()
+    setUser(newUser)
+  }, [])
 
   return (
     <div id="profile-container">
