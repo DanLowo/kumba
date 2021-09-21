f8d3cdf7826465e075c144632774178f8fdd44ed
DanLowo <danlowo@gmail.com>
2021-09-21 13:04:10 +0100

    Navigation drawer component

---
+++ src/Components/NavigationDrawer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavigationDrawer = () => {
+  return (
+    <div id="navigation-drawer">
+      <h1>Welcome</h1>
+    </div>
+  )
+}
+
+export default NavigationDrawer;
