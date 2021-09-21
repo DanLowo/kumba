cb303d91e9edb8032af7b7d38c1570504034e9ac
DanLowo <danlowo@gmail.com>
2021-09-21 14:36:40 +0100

    add navbar to site, loading component..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom'
-
 import './App.css';
 
 // import containers
-import ProfileContainer from "./Containers/Profile";
-import OrdersContainer from "./Containers/Profile/Orders";
+import ProfileContainer from "./Containers/Dashboard/Profile";
+import OrdersContainer from "./Containers/Dashboard/Orders";
+
+// import components
+import NavBarComponent from "./Components/NavBar";
+
+import LoadingImage from './asset/loading.webp'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  // remove loading after 5 seconds
+  useEffect(() => {
+    setTimeout(function () {
+      setLoading(false)
+    }, 5000)
+  })
+
   return (
-    <Switch>
-      <Route exact path="/" component={ProfileContainer} />
-      <Route path="/orders" component={OrdersContainer} />
-    </Switch>
+    <div>
+      {
+        loading ? (
+          <div id="loading-container">
+            <img alt="Loading..." src={LoadingImage} id="loading-image" />
+          </div>
+        ) : (
+          <>
+            <NavBarComponent />
+            <div id="dashboard">
+              <div>
+                <Switch>
+                  <Route exact path="/" component={ProfileContainer} />
+                  <Route path="/orders" component={OrdersContainer} />
+                </Switch>
+              </div>
+            </div>
+          </>
+        )
+      }
+    </div>
   );
   
 }
