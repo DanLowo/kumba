a21661bddbc0b5e215b583a6dd7324b4bc1be9e9
DanLowo <danlowo@gmail.com>
2021-09-21 15:31:50 +0100

    resolve button component warning

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Button = ({text, route, icon, iconPosition, dark, fullWidth}) => {
   return (
     <Link to={route}>
-      <button className={`${dark && 'dark' || ''} ${fullWidth && 'fullWidth' || ''}`}>
+      <button className={`${(dark && 'dark') || ''} ${(fullWidth && 'fullWidth') || ''}`}>
         {iconPosition === 'left' && <span className={`${icon} icon`}/>}
         {text}
         {iconPosition === 'right' && <span className={`${icon} icon`}/>}
