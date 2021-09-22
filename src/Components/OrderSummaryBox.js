0975c621fd3c1e34c86f414cd40a368ab68039ef
DanLowo <danlowo@gmail.com>
2021-09-22 19:33:36 +0100

    add money symbol if summary is money

--- src/Components/OrderSummaryBox.js
+++ src/Components/OrderSummaryBox.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const OrderSummaryBox = ({title, value, icon}) => {
+const OrderSummaryBox = ({title, value, icon, moneyIcon}) => {
   return (
     <div className="order-summary-box">
       <span className={icon} />
       <div>
-        <h2>{value}</h2>
+        <h2 className={moneyIcon && "fas fa-rupee-sign"}>{value}</h2>
         <p>{title}</p>
       </div>
     </div>
