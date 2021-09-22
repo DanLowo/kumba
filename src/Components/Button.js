38363cbcf02eb2c5e8ecf8dff11cc48e16f36811
DanLowo <danlowo@gmail.com>
2021-09-22 18:15:22 +0100

    add onClick and disabled props to button, remove link when not needed

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import {Link} from 'react-router-dom'
 
-const Button = ({text, route, icon, iconPosition, dark, fullWidth}) => {
+const Button = ({text, route, icon, iconPosition, dark, fullWidth, onClick, disabled}) => {
   return (
-    <Link to={route}>
-      <button className={`${(dark && 'dark') || ''} ${(fullWidth && 'fullWidth') || ''}`}>
-        {iconPosition === 'left' && <span className={`${icon} icon`}/>}
-        {text}
-        {iconPosition === 'right' && <span className={`${icon} icon`}/>}
-      </button>
-    </Link>
+    <Fragment>
+    {route ? (
+        <Link to={route}>
+          <button className={`${(dark && 'dark') || ''} ${(fullWidth && 'fullWidth') || ''}`}>
+            {iconPosition === 'left' && <span className={`${icon} icon`}/>}
+            {text}
+            {iconPosition === 'right' && <span className={`${icon} icon`}/>}
+          </button>
+        </Link>
+      )
+      : (
+            <button style={disabled ? {backgroundColor: 'lightgrey', borderColor: 'lightgrey'} : {}} disabled={disabled} onClick={onClick} className={`${(dark && 'dark') || ''} ${(fullWidth && 'fullWidth') || ''}`}>
+              {iconPosition === 'left' && <span className={`${icon} icon`}/>}
+              {text}
+              {iconPosition === 'right' && <span className={`${icon} icon`}/>}
+            </button>
+        )
+    }
+      </Fragment>
   )
 }
 
