d0c5a54982a71d54e238728d91bfc26c1f3dc11f
DanLowo <danlowo@gmail.com>
2021-09-22 19:34:39 +0100

    get data from api and process

--- src/Containers/Dashboard/Orders.js
+++ src/Containers/Dashboard/Orders.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 // import components
 import OrderSummaryBox from "../../Components/OrderSummaryBox";
-import Data from '../../asset/assignment.json'
 import Table from "../../Components/Table";
 import Button from "../../Components/Button";
 
 const OrdersContainer = () => {
 
+  const [{ restaurant, totalMoneySpent, totalTaxPaid, totalOrders }, setOrderDetails] = useState({
+    restaurant: {},
+    totalMoneySpent: 0,
+    totalTaxPaid: 0,
+    totalOrders: 0
+  })
+
   // state to return table headers list and table body items
-  const [{ tableHeaders, tableItems, count }, setTableData] = useState({
+  const [{ tableHeaders, tableItems, count, constItems }, setTableData] = useState({
     tableHeaders: ['Name', 'Category', 'Price', 'Quantity', 'Tax (%)', 'Total Price (INR)'],
-    tableItems: [...Data.items],
+    tableItems: [],
+    constItems: [],
     count: 1
   })
 
@@ -19,6 +27,50 @@ const OrdersContainer = () => {
     disableBack: true,
     disableNext: tableItems.length <= 7
   })
+
+  const getTotalData = (items) => {
+    let totalMoneySpent = 0
+    let totalTaxPaid = 0
+    const totalOrders = items.length
+
+    items.map(item => {
+      const { price, tax_pct: tax } = item
+      totalMoneySpent += price
+      totalTaxPaid += (price * (tax/100))
+    })
+    return { totalMoneySpent, totalTaxPaid, totalOrders }
+  }
+
+  const getAndSetData = async () => {
+    try {
+      const { data: { restaurant, items } } = await axios.get('https://indapi.kumba.io/webdev/assignment')
+
+      // set items for table
+      setTableData(prevState => ({
+        ...prevState,
+        tableItems: items,
+        constItems: items,
+        count: 1
+      }))
+
+      const { totalMoneySpent, totalTaxPaid, totalOrders } = getTotalData(items)
+      setOrderDetails(prevState => ({
+        ...prevState,
+        totalMoneySpent,
+        totalTaxPaid,
+        totalOrders,
+        restaurant
+      }))
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // eslint-disable-next-line
+  useEffect(async () => {
+      await getAndSetData()
+  }, [])
 
   const nextTable = (currentCount, data = []) => {
     const next = (currentCount * 7) - 1
@@ -72,9 +124,9 @@ const OrdersContainer = () => {
   const ControlButton = () => {
     return (
       <div id="actions">
-        <Button disabled={disableBack} text="Back" iconPosition="left" dark icon="fas fa-chevron-left" onClick={() => backTable(count, Data.items)} />
+        <Button disabled={disableBack} text="Back" iconPosition="left" dark icon="fas fa-chevron-left" onClick={() => backTable(count, constItems)} />
         <span className="count">{count}</span>
-        <Button disabled={disableNext} text="Next" iconPosition="right" dark icon="fas fa-chevron-right" onClick={() => nextTable(count, Data.items)} />
+        <Button disabled={disableNext} text="Next" iconPosition="right" dark icon="fas fa-chevron-right" onClick={() => nextTable(count, constItems)} />
       </div>
     )
   }
@@ -85,9 +137,9 @@ const OrdersContainer = () => {
       <section id="overview">
         <h3 className="section-title">Overview</h3>
         <div id="order-summary">
-          <OrderSummaryBox title="Total Order Placed" value={Data.items.length} icon="fas fa-utensils" />
-          <OrderSummaryBox title="Total Money Spent" value="$1,500" icon="fas fa-money-bill-wave" />
-          <OrderSummaryBox title="Total Tax Paid" value="$120" icon="fas fa-lightbulb" />
+          <OrderSummaryBox title="Total Order Placed" value={totalOrders} icon="fas fa-utensils" />
+          <OrderSummaryBox title="Total Money Spent" value={totalMoneySpent} moneyIcon icon="fas fa-money-bill-wave" />
+          <OrderSummaryBox title="Total Tax Paid" value={totalTaxPaid} moneyIcon icon="fas fa-lightbulb" />
         </div>
       </section>
 
